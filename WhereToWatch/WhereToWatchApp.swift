34d4112515030bf3d9a020a894aaa88920f2c9f4
Sebastian Rubina <sebastian.rubina@icloud.com>
2024-08-25 01:10:47 -0400

    Initial Commit

---
+++ WhereToWatch/WhereToWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhereToWatchApp.swift
+//  WhereToWatch
+//
+//  Created by Sebastián Rubina on 2024-08-25.
+//
+
+import SwiftUI
+
+@main
+struct WhereToWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
